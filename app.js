4cde7074b2f39fd08e74e268ac5c9c4c79fb5f55
Moshe <mobgadol@gmail.com>
2022-06-24 18:44:38 -0400

    added npm

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
+console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
